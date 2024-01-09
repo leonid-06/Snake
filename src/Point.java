4ac6453df9a3f250dd28a03a98de132886209a70
leonid-06 <harinlenya1488@gmail.com>
2024-01-09 18:27:26 +0200

    add method equals for correct comparison of Point instance

--- src/Point.java
+++ src/Point.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Point {
     private int x;
     private int y;
@@ -39,4 +41,16 @@ public class Point {
         this.y--;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return x == point.x && y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
 }
